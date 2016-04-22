2c39534f7e6b5e6a97e3d226ba3e971133cf6afd
Paul Iogna <paul.iogna@gmail.com>
2016-04-22 14:33:51 -0400

    add complete? method to project to determine if student has completed all activities.

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,6 +3,12 @@ class Project < Section
 
   before_save :set_slug
 
+  def complete?(user)
+    self.activities.each do |activity|
+      return false unless user.completed_activity(activity)
+    end
+  end
+
   private
 
   def set_slug
