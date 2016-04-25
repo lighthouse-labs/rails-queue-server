40cc33029b7db414c46f1edc1e3c5ace09321b59
Paul Iogna <paul.iogna@gmail.com>
2016-04-25 13:40:21 -0400

    remove project_submission_uploader and add migration to remove project_submission from evaluations

--- app/models/evaluation.rb
+++ app/models/evaluation.rb
@@ -1,7 +1,5 @@
 class Evaluation < ActiveRecord::Base
 
-  mount_uploader :project_submission, ProjectSubmissionUploader
-
   belongs_to :project
 
   belongs_to :student
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160422195354) do
+ActiveRecord::Schema.define(version: 20160425173607) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -166,9 +166,8 @@ ActiveRecord::Schema.define(version: 20160422195354) do
     t.integer  "student_id"
     t.integer  "teacher_id"
     t.boolean  "accepted"
-    t.datetime "created_at",         null: false
-    t.datetime "updated_at",         null: false
-    t.string   "project_submission"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "feedbacks", force: :cascade do |t|
