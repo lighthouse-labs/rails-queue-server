55342b951b913e24685bd354d5735844a0df7ddd
Paul Iogna <paul.iogna@gmail.com>
2016-04-25 13:41:50 -0400

    add migration to add url to evalations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160425173607) do
+ActiveRecord::Schema.define(version: 20160425174105) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -168,6 +168,7 @@ ActiveRecord::Schema.define(version: 20160425173607) do
     t.boolean  "accepted"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "url"
   end
 
   create_table "feedbacks", force: :cascade do |t|
