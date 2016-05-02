f232f4b5db104b4169e94116744bef0eb62e2073
Rafal Dittwald <rafal.dittwald@gmail.com>
2016-05-01 23:28:21 -0400

    update prep seed to fetch outcomes from github repo YAML frontmatter

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,16 +7,52 @@ def comma
   print ','; STDOUT.flush
 end
 
+# Note: assumed that you (your github profile) will have access to this curriculum content repo
+#       and have set your GITHUB_ADMIN_OAUTH_TOKEN in the .env file
+$github_username = "lighthouse-labs"
+$github_repo = "2016-web-curriculum-activities"
+$github_ref = "master"
+
+class ExternalResource
+  @@client = nil
+
+  def self.init
+    return @@client if @@client
+    @@client = Octokit::Client.new(access_token: ENV['GITHUB_ADMIN_OAUTH_TOKEN'])
+    @@client.login
+    @@client
+  end
+
+  def self.fetch(file_path)
+    if file_path
+      self.init
+      begin
+        return @@client.contents($github_username+"/"+$github_repo, path: file_path, accept: 'application/vnd.github.V3.raw', ref: $github_ref)
+      rescue Octokit::NotFound => e
+        puts "NOT FOUND:\n#{file_path}"
+      end
+    end
+    return nil
+  end
+
+  def self.fetch_frontmatter(file_path)
+    if raw_content = self.fetch(file_path)
+      if matches = raw_content.lstrip.match(/---(.*)---.*/m)
+        return YAML.load(matches[1])
+      end
+    end
+    return {}
+  end
+end
+
 # Real shit
 @program = Program.find_or_create_by!(name: "Web Immersive")
 @location_van = Location.find_or_create_by!(name: "Vancouver")
 @location_to = Location.find_or_create_by!(name: "Toronto")
 
-# Note: assumed that you (your github profile) will have access to this curriculum content repo
-#       and have set your GITHUB_ADMIN_OAUTH_TOKEN in the .env file
 @repo = ContentRepository.find_or_create_by!(
-  github_username: 'lighthouse-labs', 
-  github_repo: '2016-web-curriculum-activities'
+  github_username: $github_username,
+  github_repo: $github_repo
 )
 
 require Rails.root.join('db/seeds/outcomes/sync').to_s
--- db/seeds/prep.rb
+++ db/seeds/prep.rb
@@ -1,11 +1,11 @@
 ########
 # PREP CONTENT SEEDING
-# This file uses the yml content in db/seeds/prep folder to 
+# This file uses the yml content in db/seeds/prep folder to
 # populate REAL curriculum content for the prep course
 #
 # It's called via the main db/seeds.rb file when running bin/rake db:seed
 # It is non-descrutive and creates/updates Prep and Activity records
-# and requires all the records to have a uuid present which it uses to 
+# and requires all the records to have a uuid present which it uses to
 # populate the db with the content from the yml files
 #   - KV
 ########
@@ -13,29 +13,39 @@
 puts "Loading prep data"
 
 # for tracking and preventing accidental dupe uuids in the data
-#   ie copy/paste mistakes 
+#   ie copy/paste mistakes
 @activity_uuids = []
 @section_uuids = []
 
-@default_activity_attributes = { 
-  remote_content: true, 
-  content_repository: ContentRepository.first, 
+@default_activity_attributes = {
+  remote_content: true,
+  content_repository: ContentRepository.first,
   duration: 15,
   allow_submissions: false,
 }
 
 def generate_activities(section, activity_data)
   activities = []
-  
+
   activity_data.each_with_index do |activity_attributes, i|
     uuid = activity_attributes['uuid']
     abort("\n\n---\nHALT! Activity UUID required") if uuid.blank?
     abort("\n\n---\nHALT! Dupe Acitivty UUID found. Check your data, as this is potentially disasterous!") if @activity_uuids.include?(uuid)
 
     attrs = @default_activity_attributes.merge(activity_attributes)
+
+    # merge YAML frontmatter from external resource
+    external_attrs = ExternalResource.fetch_frontmatter(attrs['content_file_path'])
+    attrs = attrs.merge(external_attrs)
+
     attrs[:start_time] = 900 + (i * 30)
+    if outcome_uuids = attrs.delete('outcomes')
+      attrs['outcomes'] = outcome_uuids.map {|uuid|
+        Outcome.find_by_uuid(uuid)
+      }.compact
+    end
     if quiz_uuid = attrs.delete('quiz')
-      attrs['quiz'] = Quiz.find_by(uuid: quiz_uuid) 
+      attrs['quiz'] = Quiz.find_by(uuid: quiz_uuid)
     end
     if activity = Activity.find_by(uuid: uuid)
       activity.activity_test.try :destroy
@@ -65,7 +75,7 @@ Dir.glob(dir).each_with_index do |file, i|
   uuid = section_data['uuid']
   abort("\n\n---\nHALT! Section UUID required") if uuid.blank?
   abort("\n\n---\nHALT! Dupe Section UUID found. Check your data, as this is potentially disasterous!") if @section_uuids.include?(uuid)
-  
+
   section_data.merge!(order: i+1)
 
   if section = Prep.find_by(uuid: section_data['uuid'])
