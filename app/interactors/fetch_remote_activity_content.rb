0a3dd98a8f88fce98e722e878e4b797ca71f85c1
Rafal Dittwald <rafal.dittwald@gmail.com>
2016-05-02 17:01:45 -0400

    handle case when file_path is nil

--- app/interactors/fetch_remote_activity_content.rb
+++ app/interactors/fetch_remote_activity_content.rb
@@ -6,10 +6,12 @@ class FetchRemoteActivityContent
     file_path = activity.content_file_path
     repo_name = activity.content_repository.full_name
 
-    content = github_client.contents(repo_name, path: file_path, accept: 'application/vnd.github.V3.raw')
-    content = content.lstrip.sub(/^---(.*?)---/m, "") # remove frontmatter
-    activity.instructions = content
-    activity.save
+    if file_path
+      content = github_client.contents(repo_name, path: file_path, accept: 'application/vnd.github.V3.raw')
+      content = content.lstrip.sub(/^---(.*?)---/m, "") # remove frontmatter
+      activity.instructions = content
+      activity.save
+    end
   rescue Octokit::NotFound => e
     puts "!! FAILED TO FETCH '#{file_path}' FROM GITHUB !!"
     context.fail! error: e.message
