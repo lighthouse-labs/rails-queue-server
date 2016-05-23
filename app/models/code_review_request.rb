37ca36e89acdc2a0418ce9ef28dac16a34a508a0
Khurram Virani <kvirani@gmail.com>
2016-05-23 10:15:02 -0700

    review and changes for James' code review fix
    Fix migration:
    - Don’t use save (callbacks, validation etc)
    - Use find_each for batch mode
    - change `change` to up/down
    - Only set `activity_id` on AR table; don’t set
    `original_activity_submission_id`. I think this new col should not be
    used (ie we keep activity_submission_Id)
    
    Model changes:
    - keep activity_submission reference on AR
    - set activity_submission before_create on CodeReviewRequest
    
    Note: Manually tested all using production db
    
    cc @interlock

--- app/models/code_review_request.rb
+++ app/models/code_review_request.rb
@@ -1,2 +1,11 @@
 class CodeReviewRequest < AssistanceRequest
+
+  before_create :set_activity_submission, if: :activity
+
+  private
+
+  def set_activity_submission
+    self.activity_submission = self.requestor.activity_submissions.where(activity: self.activity).order(created_at: :desc).first
+  end
+
 end
--- db/migrate/20160505170745_assistance_request_submission_to_activity.rb
+++ db/migrate/20160505170745_assistance_request_submission_to_activity.rb
@@ -1,17 +1,23 @@
 class AssistanceRequestSubmissionToActivity < ActiveRecord::Migration
-  def change
-    # Looks like AR does not support UPDATE with joins, so we batch this!
-    batch_size = 100
-    begin
-      assistance_requests = AssistanceRequest.where.not(activity_submission_id: nil).limit(batch_size)
-      assistance_requests.each do |ar|
-        ar.update!({
-          activity_id: ar.activity_submission.activity_id,
-          original_activity_submission_id: ar.activity_submission.id,
-          activity_submission_id: nil
-        })
-      end
-    end until assistance_requests.empty?
 
+  def up
+    requests = AssistanceRequest.where.not(activity_submission_id: nil).where(activity_id: nil)
+    puts "Updating #{requests.count} requests: "
+    requests.find_each(batch_size: 500) do |ar|
+      # submission is needed for activity_id
+      if sub = ar.activity_submission
+        # use update_all so as to not trigger .save so as to not do validation nor modify updated_at field
+        AssistanceRequest.where(id: ar.id).update_all(activity_id: sub.activity_id)
+        print '.'; STDOUT.flush
+      else
+        print "[#{ar.id}]"; STDOUT.flush
+      end
+    end
+    puts "\nDONE!"
   end
+
+  def down
+    AssistanceRequest.update_all(activity_id: nil)
+  end
+
 end
