f129a4276768d65ddcbd418c78fc4e082630b540
Paul Iogna <paul.iogna@gmail.com>
2016-05-31 14:10:11 -0400

    add tab for prep. add prep_activity_stats method to student_presenter

--- app/presenters/student_presenter.rb
+++ app/presenters/student_presenter.rb
@@ -24,6 +24,18 @@ class StudentPresenter < UserPresenter
     result.html_safe
   end
 
+  def prep_activity_stats
+    completed_activities = student.activity_submissions.joins(:activity, :section).where("sections.type = 'Prep'").count
+    total_activities = Activity.joins(:section).where("sections.type = 'Prep'").count
+    activity_ratio = completed_activities > 0 ? (completed_activities.to_f / total_activities) * 100 : 0
+    result = "<div class='stats'><p>#{completed_activities} of #{total_activities} activities</p><p>&nbsp</p>"
+    result += "<div class='progress'><div class='progress-bar' role='progressbar'"
+    result += "aria-valuenow='#{activity_ratio}' aria-valuemin='0' aria-valuemax='100' style='width:#{activity_ratio}%'>"
+    result += "<span class='sr-only'>#{activity_ratio}% Complete</span></div></div>"
+    result += "<p>How goes</p></div>"
+    result.html_safe
+  end
+
   def activity_stats
     completed_activities = student.activity_submissions.count
     total_activities = Activity.count
@@ -40,7 +52,7 @@ class StudentPresenter < UserPresenter
     quiz_submissions = student.quiz_submissions.where(initial: true).count
     total_quizzes = Quiz.count
     quiz_ratio = quiz_submissions > 0 ? (quiz_submissions.to_f / total_quizzes) * 100 : 0
-    quiz_average = (student.quiz_submissions.inject(0.0) { |total, submission| total + (submission.score.to_f / submission.quiz.questions.count)} / student.quiz_submissions.count * 100).to_i
+    quiz_average = student.quiz_submissions.count > 0 ? (student.quiz_submissions.inject(0.0) { |total, submission| total + (submission.score.to_f / submission.quiz.questions.count)} / student.quiz_submissions.count * 100).to_i : 0
     result = "<div class='stats'><p>#{quiz_submissions} of #{total_quizzes} quizzes</p>"
     result += "<p>Average: #{quiz_average}</p>"
     result += "<div class='progress'><div class='progress-bar' role='progressbar'"
