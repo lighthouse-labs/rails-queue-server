69afd74ebe098c43bdf83b53fd524067363f42ff
sjhogan2 <shannon.jane.hogan@gmail.com>
2016-08-24 22:33:06 +0000

    206 Remove unused test files

--- spec/factories/prep_assistance_requests.rb
+++
@@ -1,7 +0,0 @@
-FactoryGirl.define do
-  factory :prep_assistance_request do
-    created_at "MyString"
- "2016-08-23 21:25:44"
-  end
-
-end
--- spec/models/prep_assistance_request_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe PrepAssistanceRequest do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
