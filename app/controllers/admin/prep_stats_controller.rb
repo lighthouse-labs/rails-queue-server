290d0e77c25e97a72f7c11687eb6cce40a24e835
Khurram Virani <kvirani@gmail.com>
2016-10-06 10:57:34 -0700

    resolves #63

--- app/controllers/admin/prep_stats_controller.rb
+++ app/controllers/admin/prep_stats_controller.rb
@@ -3,7 +3,7 @@ class Admin::PrepStatsController < ApplicationController
   DEFAULT_PER = 10
 
   def index
-    @users = User.all
+    @users = User.active
     @users = @users.where(id: params[:user_id]) if params[:user_id]
     @users = @users.page(params[:page]).per(DEFAULT_PER)
   end
