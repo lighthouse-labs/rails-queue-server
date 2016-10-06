5cab6e6fa57b5ba90614675c1f7f3f130d985a14
sjhogan2 <shannon.jane.hogan@gmail.com>
2016-10-06 10:07:17 -0700

    Add conditionals for default sorting
    If no sort params are specified, defaults to start of month, end of
    month, and teacher’s location to speed up load speed of page.

--- app/controllers/admin/teacher_feedbacks_controller.rb
+++ app/controllers/admin/teacher_feedbacks_controller.rb
@@ -6,6 +6,15 @@ class Admin::TeacherFeedbacksController < Admin::BaseController
     if params[:teacher_location_id].nil?
       params[:teacher_location_id] = current_user.location.id.to_s  
     end
+
+    if params[:start_date].nil?
+      params[:start_date] = Date.today.beginning_of_month.to_s
+    end
+
+    if params[:end_date].nil?
+      params[:end_date] = Date.today.end_of_month.to_s
+    end
+
     @feedbacks = Feedback.teacher_feedbacks.filter_by(filter_by_params)
     @completed_feedbacks = Feedback.teacher_feedbacks.completed.filter_by(filter_by_params).group_by(&:teacher)
   end
