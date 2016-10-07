2d2d3ce383ee6ff1d23d2d815c675e24d3ba9974
Khurram Virani <kvirani@gmail.com>
2016-10-07 15:00:31 -0700

    disable prep assistance
    for pt course

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161007155701) do
+ActiveRecord::Schema.define(version: 20161007215704) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -355,6 +355,7 @@ ActiveRecord::Schema.define(version: 20161007155701) do
     t.boolean  "has_projects",                    default: true
     t.boolean  "has_code_reviews",                default: true
     t.boolean  "display_exact_activity_duration"
+    t.boolean  "prep_assistance"
   end
 
   create_table "questions", force: :cascade do |t|
