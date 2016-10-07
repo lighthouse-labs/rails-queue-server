808f3341142c5a37244a04a766fc09919987ee7e
Khurram Virani <kvirani@gmail.com>
2016-10-07 09:27:29 -0700

    append -1 for rollover students
    for assessment tests, as per temporary workaround to uniqueness
    validation in proctologist, where a github username can only take a
    test once. Handles rollover situation.

--- app/helpers/assessment_tests_helper.rb
+++ app/helpers/assessment_tests_helper.rb
@@ -1,7 +1,13 @@
 module AssessmentTestsHelper
 
   def url_for_proctologist(test, cohort)
-    student_usernames = cohort.students.active.pluck :github_username
+    student_usernames = cohort.students.active.map do |s|
+      if s.rolled_in?(cohort)
+        "#{s.github_username}-1"
+      else
+        s.github_username
+      end
+    end
     student_usernames = student_usernames.join(',')
     "#{ENV['PROCTOLOGIST_URL']}admin/exams/#{test}/?studentIds=#{student_usernames}"
   end
