9e2fd4d8e9461b85086aac7aeb2c9ef46ff03ab8
sjhogan2 <shannon.jane.hogan@gmail.com>
2016-10-07 10:33:16 -0700

    Add if statement to handle cases where outcomes == nil

--- app/interactors/create_outcome_results_from_evaluation.rb
+++ app/interactors/create_outcome_results_from_evaluation.rb
@@ -3,14 +3,16 @@ class CreateOutcomeResultsFromEvaluation
 
   def call
     outcomes = context.evaluation_form[:outcomes]
-    OutcomeResult.transaction do
-      outcomes.each do |key, outcome|
-        OutcomeResult.create!(outcome_id: outcome[:id],
-                             rating: outcome[:mark],
-                             source_id: outcome[:activity_id],
-                             source_type: "Activity",
-                             user: context.evaluation.student
-                             ) if outcome[:mark].present?
+    if outcomes
+      OutcomeResult.transaction do
+        outcomes.each do |key, outcome|
+          OutcomeResult.create!(outcome_id: outcome[:id],
+                               rating: outcome[:mark],
+                               source_id: outcome[:activity_id],
+                               source_type: "Activity",
+                               user: context.evaluation.student
+                               ) if outcome[:mark].present?
+        end
       end
     end
     rescue ActiveRecord::RecordInvalid => exception
