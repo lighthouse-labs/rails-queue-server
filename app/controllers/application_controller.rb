00fd4056e6944704669b879c4aea99ef24765a5b
James Sapara <james.sapara@gmail.com>
2016-10-18 18:43:52 -0700

    added users.auth_token field and support to authenticate with x-auth-token header that matches a not empty token for users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  # protect_from_forgery with: :exception
   before_action :authenticate_user
   before_action :set_timezone
-  before_filter :set_raven_context
+  before_action :set_raven_context
 
   private
 
@@ -28,6 +28,7 @@ class ApplicationController < ActionController::Base
   end
 
   def current_user
+    @current_user ||= User.find_by_auth_token(request.headers[:'x-auth-token']) if request.headers[:'x-auth-token'].present?
     @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
     cookies.signed[:user_id] = @current_user.id if @current_user && cookies.signed[:user_id].blank?
 
