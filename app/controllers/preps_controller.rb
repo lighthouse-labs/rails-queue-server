76ed570d5e27ab018bd51010cf29fb608218fe2d
sjhogan2 <shannon.jane.hogan@gmail.com>
2016-10-18 17:16:26 -0700

    Refactor code for readability

--- app/controllers/preps_controller.rb
+++ app/controllers/preps_controller.rb
@@ -11,7 +11,7 @@ class PrepsController < ApplicationController
   private
 
   def redirect_to_first_activity
-    if params[:id].present? && !Prep.find_by(slug: params[:id]).nil?
+    if params[:id].present? && Prep.find_by(slug: params[:id])
       @prep = Prep.find_by(slug: params[:id])
     else
       @prep = Prep.first
