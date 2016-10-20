8dc5ef6ef5fc716e272758ea8426f51aeea77d29
sjhogan2 <shannon.jane.hogan@gmail.com>
2016-10-20 16:51:22 -0700

    Refactor to display error message rather than redirect in case of faulty params[:id]

--- app/controllers/preps_controller.rb
+++ app/controllers/preps_controller.rb
@@ -11,11 +11,7 @@ class PrepsController < ApplicationController
   private
 
   def redirect_to_first_activity
-    if params[:id].present? && Prep.find_by(slug: params[:id])
-      @prep = Prep.find_by(slug: params[:id])
-    else
-      @prep = Prep.first
-    end
+    @prep = params[:id].present? ? Prep.find_by!(slug: params[:id]) : Prep.first
     @activity = @prep.activities.chronological.active.first
     redirect_to prep_activity_path(@prep, @activity)
   end
