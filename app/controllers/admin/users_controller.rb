c3bca9a549e6e75828bd6a6cd73016f256838cb3
Khurram Virani <kvirani@gmail.com>
2016-11-06 20:26:31 -0800

    Improvements to admin users index page
    - Improve n+1 query issue (perf)
      - Was seeing hundreds of (albeit cached) AR queries in log. Now down
    to double digits.
    - Paginate results with kaminari (perf)
    - Added and implemented search filters
    - Made index page show
    - Added links to github, slack, etc (new col)
    - Added yellow label under “Type” col to denote admin users
    - Changed status from “dropped” to “deactivated” since user can be of
    non-student type and “dropped” is confusing for teachers, admins, etc.
    
    refs #41; cc @sjhogan2

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,8 +2,11 @@ class Admin::UsersController < Admin::BaseController
 
   before_action :load_user, only: [:reactivate, :deactivate]
 
+  DEFAULT_PER = 25
+
   def index
-    @users = User.all
+    @users = User.all.includes(:location, :cohort).page(params[:page]).per(DEFAULT_PER)
+    apply_filters
   end
 
   def reactivate
@@ -20,6 +23,56 @@ class Admin::UsersController < Admin::BaseController
 
   def load_user
     @user = User.find(params[:id])
+  end
+
+  def apply_filters
+    filter_by_status
+    filter_by_type
+    filter_by_location
+    filter_by_admin
+  end
+
+  def filter_by_status
+    params[:status] ||= 'All'
+    @users = case params[:status]
+    when 'Active'
+      @users.active
+    when 'Deactivated'
+      @users.deactivated
+    else
+      @users
+    end
+  end
+
+  def filter_by_type
+    params[:type] ||= 'All'
+    @users = case params[:type]
+    when 'Teacher'
+      @users.where(type: 'Teacher')
+    when 'Student'
+      @users.where(type: 'Student').where.not(cohort_id: nil)
+    when 'Prep Only'
+      @users.where(type: [nil, 'Student']).where(cohort_id: nil)
+    else
+      @users
+    end
+  end
+
+  def filter_by_location
+    return unless params[:location_id].present?
+    @users = @users.where(location_id: params[:location_id])
+  end
+
+  def filter_by_admin
+    params[:admin] ||= 'Include'
+    @users = case params[:admin]
+    when 'Exclude'
+      @users.where(admin: [nil, false])
+    when 'Only'
+      @users.where(admin: true)
+    else
+      @users
+    end
   end
 
 end
