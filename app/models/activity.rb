3635729fa913b24941ed5a4312c5a0bc05aeaa17
Khurram Virani <kvirani@gmail.com>
2017-03-10 17:47:45 -0800

    Add assistance requests to admin/activities report
    Added new column with number of assistance requests by activity.
    
    Will help us see which ones require the most help.
    
    The count is only legitimate, finished (non-cancelled) requests.

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -6,6 +6,7 @@ class Activity < ApplicationRecord
 
   has_many :activity_submissions, -> { order(:user_id) }
   has_many :assistances, -> { order(:user_id)}
+  has_many :assistance_requests
   has_many :messages, -> { order(created_at: :desc) }, class_name: 'ActivityMessage'
   has_many :recordings, -> { order(created_at: :desc) }
   has_many :feedbacks, as: :feedbackable
