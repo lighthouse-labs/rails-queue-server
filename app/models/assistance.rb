f033e205ccbf0ddea952cd262ee146e0a713291d
wesley-wong <wes.wong@hotmail.com>
2017-06-20 16:10:25 -0700

    allows you to search assistances using student info with .by_student_keywords() method

--- app/models/assistance.rb
+++ app/models/assistance.rb
@@ -1,9 +1,23 @@
 class Assistance < ApplicationRecord
 
+  include PgSearch
+
   belongs_to :assistor, :class_name => User
   belongs_to :assistee, :class_name => User
   belongs_to :activity
   belongs_to :cohort # substitute for lack of enrollment record - KV
+
+  pg_search_scope :by_student_keywords,
+    :associated_against => {
+      :assistee => [:first_name, :last_name, :email],
+    },
+    using: {
+      tsearch: {
+        dictionary: "english",
+        any_word:   true,
+        prefix:     true
+      }
+    }
 
   has_one :feedback, as: :feedbackable, dependent: :destroy
   has_one :assistance_request, dependent: :nullify
