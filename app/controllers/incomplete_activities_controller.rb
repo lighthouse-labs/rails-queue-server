4b04f7069986479334c4e00a1640214c53ac81bf
wesley-wong <wes.wong@hotmail.com>
2017-06-26 17:01:40 -0700

    Display incomplete vs stretch activities for student

--- app/controllers/incomplete_activities_controller.rb
+++ app/controllers/incomplete_activities_controller.rb
@@ -4,12 +4,14 @@ class IncompleteActivitiesController < ApplicationController
 
   def index
     @activities = current_user.incomplete_activities
+    @incomplete_and_not_stretch = @activities.where(stretch: nil).count
+    @incomplete_and_stretch = @activities.count(:stretch);
   end
 
   private
 
   def student_required
-    redirect_to(:root, alert: 'Not allowed') unless student?
+    redirect_to(:root, alert: 'You are not a student') unless student?
   end
 
 end
