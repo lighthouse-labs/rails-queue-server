d0305ad4c3cae235360091ee0cfde66c5980c5e3
wesley-wong <wes.wong@hotmail.com>
2017-07-11 16:10:43 -0700

    rubocop -a

--- app/models/user.rb
+++ app/models/user.rb
@@ -160,7 +160,7 @@ class User < ApplicationRecord
   end
 
   def incomplete_activities
-    Activity.where.not(id: self.activity_submissions.select(:activity_id)).where("day <= ?", CurriculumDay.new(Time.zone.yesterday, cohort).to_s).order(day: :desc)
+    Activity.where.not(id: activity_submissions.select(:activity_id)).where("day <= ?", CurriculumDay.new(Time.zone.yesterday, cohort).to_s).order(day: :desc)
   end
 
   def completed_code_reviews
