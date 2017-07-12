3ab74f8a59f6266f84a1b2a3b6d0622973ee113b
wesley-wong <wes.wong@hotmail.com>
2017-07-12 09:25:53 -0700

    removed instance variable in line with review

--- app/controllers/incomplete_activities_controller.rb
+++ app/controllers/incomplete_activities_controller.rb
@@ -4,7 +4,6 @@ class IncompleteActivitiesController < ApplicationController
 
   def index
     @activities = current_user.incomplete_activities
-    @incomplete_and_stretch = @activities.stretch
   end
 
   private
