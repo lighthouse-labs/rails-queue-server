79c8b69939cce5acd115758d456314a6f1047232
wesley-wong <wes.wong@hotmail.com>
2017-09-08 13:32:44 -0700

    revert regex change in cohort model, change faker code in cohort factory

--- spec/factories/cohorts.rb
+++ spec/factories/cohorts.rb
@@ -4,7 +4,7 @@ FactoryGirl.define do
   factory :cohort do
     name { Faker::Name.name }
     start_date { Date.current }
-    code { SecureRandom.hex(10) }
+    code { Faker::Name.first_name.downcase + '-' + Faker::Name.last_name.downcase }
     association :location
     association :program
   end
