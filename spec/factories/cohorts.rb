2c9eb414305a810538b0a2782b014f92fc98e152
wesley-wong <wes.wong@hotmail.com>
2017-09-11 09:41:21 -0700

    better cohort code

--- spec/factories/cohorts.rb
+++ spec/factories/cohorts.rb
@@ -4,7 +4,7 @@ FactoryGirl.define do
   factory :cohort do
     name { Faker::Name.name }
     start_date { Date.current }
-    code { Faker::Name.first_name.downcase + '-' + Faker::Name.last_name.downcase }
+    code { Faker::Superhero.name.parameterize }
     association :location
     association :program
   end
