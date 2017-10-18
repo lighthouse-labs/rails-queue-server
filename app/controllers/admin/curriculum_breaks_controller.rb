ad3f284c0ab6085f1657982804731924c8f612b1
James Matsuba <james.matsuba@gmail.com>
2017-10-17 17:00:18 -0700

    more crud views

--- app/controllers/admin/curriculum_breaks_controller.rb
+++ app/controllers/admin/curriculum_breaks_controller.rb
@@ -4,12 +4,31 @@ class Admin::CurriculumBreaksController < Admin::BaseController
   before_action :require_curriculum_break, only: [:edit, :update, :show]
 
   def index
+    @curriculum_breaks = CurriculumBreak.for_cohort(@cohort)
   end
 
   def new
+    @curriculum_break = CurriculumBreak.new(cohort: @cohort)
   end
 
   def show
+  end
+
+  def create
+    @curriculum_break = CurriculumBreak.new(curriculum_break_params)
+    if @curriculum_break.save
+      redirect_to admin_cohort_curriculum_breaks_path(@cohort), notice: 'Created!'
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @curriculum_break.update(curriculum_break_params)
+      redirect_to admin_cohort_curriculum_breaks_path(@cohort), notice: 'Updated!'
+    else
+      render :edit
+    end
   end
 
   private
--- app/models/curriculum_break.rb
+++ app/models/curriculum_break.rb
@@ -1,5 +1,6 @@
 class CurriculumBreak < ApplicationRecord
 
   belongs_to :cohort
+  scope :for_cohort, -> (cohort) { where cohort_id: cohort.id }
 
 end
