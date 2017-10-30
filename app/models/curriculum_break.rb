d878fecb0a08183184f6e25edfb88a547f28dda9
James Matsuba <james.matsuba@gmail.com>
2017-10-30 11:44:25 -0700

    added validation for mondays and in the cohort for starts_on for CurriculumBreak

--- app/models/curriculum_break.rb
+++ app/models/curriculum_break.rb
@@ -1,9 +1,17 @@
 class CurriculumBreak < ApplicationRecord
 
   belongs_to :cohort
-
   validates :reason, :starts_on, :num_weeks, presence: true
+  validate :starts_on_monday
+  validate :within_timeframe
 
-  #Validation Required: Starts on Mondays, They do not overlap
+  def starts_on_monday
+    errors.add(:starts_on, "breaks need to start on a monday") unless starts_on.monday?
+  end
+
+  def within_timeframe
+    cohort_range = cohort.start_date..(cohort.start_date + cohort.program.weeks.weeks)
+    errors.add(:starts_on, "needs to be during the cohort") unless cohort_range.cover?(starts_on)
+  end
 
 end
