53b256712a3edc2c05d45393cca60f0f07776c70
Alex Hache <ahache8@gmail.com>
2017-11-15 16:57:19 -0800

    Removed some dead code

--- db/migrate/20171111001233_add_current_assistance_average_to_users.rb
+++ db/migrate/20171111001233_add_current_assistance_average_to_users.rb
@@ -1,7 +1,6 @@
 class AddCurrentAssistanceAverageToUsers < ActiveRecord::Migration[5.0]
   def change
     Student.find_each(batch_size: 20) do |s|
-      # Student.where(id: s.id).update()
       s.assistance_average = StudentStats.new(s).bootcamp_assistance_stats[:average_score]
       s.save!
     end
