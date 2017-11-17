b244e546b961429f214bf7f17d4110fc83845596
Alex Hache <ahache8@gmail.com>
2017-11-16 17:34:43 -0800

    Changed batch size

--- db/migrate/20171111001233_add_current_assistance_average_to_users.rb
+++ db/migrate/20171111001233_add_current_assistance_average_to_users.rb
@@ -1,6 +1,6 @@
 class AddCurrentAssistanceAverageToUsers < ActiveRecord::Migration[5.0]
   def change
-    Student.find_each(batch_size: 20) do |s|
+    Student.find_each(batch_size: 100) do |s|
       s.assistance_average = StudentStats.new(s).bootcamp_assistance_stats[:average_score]
       s.save!
     end
