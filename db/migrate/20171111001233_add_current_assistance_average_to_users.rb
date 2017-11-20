0bf9599a9d61c22e04f07488b5a2e6c6e67f3ccd
Alex Hache <ahache8@gmail.com>
2017-11-20 11:48:19 -0800

    Quick fix for migration query

--- db/migrate/20171111001233_add_current_assistance_average_to_users.rb
+++ db/migrate/20171111001233_add_current_assistance_average_to_users.rb
@@ -1,7 +1,7 @@
 class AddCurrentAssistanceAverageToUsers < ActiveRecord::Migration[5.0]
   def up
     Student.find_each(batch_size: 100) do |s|
-      s.cohort_assistance_average = s.assistances.completed.where(cohort_id: cohort_id).where.not(rating: nil).average(:rating).to_f.round(2)
+      s.cohort_assistance_average = s.assistances.completed.where(cohort_id: s.cohort_id).where.not(rating: nil).average(:rating).to_f.round(2)
       s.save!
       puts "updated #{s.first_name} id: #{s.id}"
     end
