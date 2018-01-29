514dfb0cef8026c94dfd7b662981bf7d3798f0f9
James Matsuba <james.matsuba@gmail.com>
2018-01-29 11:20:10 -0800

    make tech interview questions seqeuncial

--- app/controllers/tech_interviews_controller.rb
+++ app/controllers/tech_interviews_controller.rb
@@ -102,7 +102,7 @@ class TechInterviewsController < ApplicationController
   def require_interview
     @tech_interview = TechInterview.find params[:id]
     @interview_template ||= @tech_interview.tech_interview_template
-    @questions = @interview_template.questions
+    @questions = @interview_template.questions.active.sequencial
   end
 
   def only_incomplete
