59061565c6c0472342f81a9a1ba15e3d20442d33
James Matsuba <james.matsuba@gmail.com>
2018-01-31 17:11:02 -0800

    refactored set archieve interactor

--- app/interactors/content/set_archive.rb
+++ app/interactors/content/set_archive.rb
@@ -2,42 +2,51 @@ class Content::SetArchive
 
   include Interactor
 
+  before do
+    @repo_data = context.repo_data
+    @model = context.model
+  end
+
   def call
-    set_archive(context.repo_data, context.model)
+    get_repo_uuids
+    get_db_items
+    set_archive
   end
 
   private
 
-  def set_archive(repo_data, model)
-    repo_uuids = []
-
-    repo_data.each do |data|
-      repo_uuids.push data['uuid']
+  def get_repo_uuids
+    @repo_uuids = []
+    @repo_data.each do |data|
+      @repo_uuids.push data['uuid']
     end
+  end
 
-    db_items = model.where(archived: [nil, false]).map{ |m| {id: m.id, uuid: m.uuid} }
+  def get_db_items
+    @db_items = @model.where(archived: [nil, false]).map{ |m| {id: m.id, uuid: m.uuid} }
+    @archived_db_items = @model.where(archived: true).map{ |m| {id: m.id, uuid: m.uuid} }
+  end
 
-    archived_db_items = model.where(archived: true).map{ |m| {id: m.id, uuid: m.uuid} }
-
-    db_items.each do |db_item|
-      if !in_repo_data(repo_uuids, db_item[:uuid])
-        archived_object = model.find_by(id: db_item[:id])
+  def set_archive
+    @db_items.each do |db_item|
+      if !in_curric_repo_data(db_item[:uuid])
+        archived_object = @model.find_by(id: db_item[:id])
         archived_object.archived = true
-        context.fail!("Failed to Set Archive for #{model.name}, id: #{db_item[:id]}") unless archived_object.save
+        context.fail!("Failed to Set Archive for #{@model.name}, id: #{db_item[:id]}") unless archived_object.save
       end
     end
 
-    archived_db_items.each do |db_item|
-      if in_repo_data(repo_uuids, db_item[:uuid])
-        archived_object = model.find_by(id: db_item[:id])
+    @archived_db_items.each do |db_item|
+      if in_curric_repo_data(db_item[:uuid])
+        archived_object = @model.find_by(id: db_item[:id])
         archived_object.archived = false
-        context.fail!("Failed to Set Archive for #{model.name}, id: #{db_item[:id]}") unless archived_object.save
+        context.fail!("Failed to Set Archive for #{@model.name}, id: #{db_item[:id]}") unless archived_object.save
       end
     end
   end
 
-  def in_repo_data(repo_uuids, db_uuid)
-    repo_uuids.include?(db_uuid)
+  def in_curric_repo_data(db_uuid)
+    @repo_uuids.include?(db_uuid)
   end
 
 end
