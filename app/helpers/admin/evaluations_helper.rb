fd01b6c6fb6c1229a0849e27dffa07a516b31ca0
Alex Hache <ahache8@gmail.com>
2018-02-05 10:21:22 -0800

    Handling empty set of evaluations

--- app/helpers/admin/evaluations_helper.rb
+++ app/helpers/admin/evaluations_helper.rb
@@ -5,12 +5,14 @@ module Admin::EvaluationsHelper
   end
 
   def total_marking_time(evaluations)
+    return '0' if evaluations.empty?
     total_seconds = evaluations.map{ |e| e.duration }.reduce(:+)
     seconds_to_formatted_time(total_seconds)
   end
 
   def average_marking_time(evaluations)
-    average_seconds = evaluations.map{ |e| e.duration }.reduce(:+) / @evaluations.count
+    return '0' if evaluations.empty?
+    average_seconds = evaluations.map{ |e| e.duration }.reduce(:+) / evaluations.count
     seconds_to_formatted_time(average_seconds)
   end
 
