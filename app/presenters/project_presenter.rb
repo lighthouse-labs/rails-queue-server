0ce3b39a559d65a7f8c512c4cdfaf39585abd8a9
Khurram Virani <kvirani@gmail.com>
2018-02-07 16:44:23 -0800

    Final (hopefully) label -> badge CSS class change (Bs4)

--- app/presenters/project_presenter.rb
+++ app/presenters/project_presenter.rb
@@ -9,13 +9,13 @@ class ProjectPresenter < BasePresenter
   def student_project_status
     project_week = project.end_day[1].to_i
     result = if cohort.week > project_week
-               content_tag(:span, 'Overdue', class: 'label label-danger')
+               content_tag(:span, 'Overdue', class: 'badge badge-danger')
              elsif cohort.week == project_week
-               content_tag(:span, 'This Week', class: 'label label-info')
+               content_tag(:span, 'This Week', class: 'badge badge-info')
              elsif cohort.week + 1 == project_week
-               content_tag(:span, 'Next Week', class: 'label label-info')
+               content_tag(:span, 'Next Week', class: 'badge badge-info')
              else
-               content_tag(:span, 'Pending', class: 'label label-info')
+               content_tag(:span, 'Pending', class: 'badge badge-info')
              end
     result
   end
