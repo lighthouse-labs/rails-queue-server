ce0324c0654a65d27b7227a6e31be50ce3af855a
Alex Hache <ahache8@gmail.com>
2018-02-12 11:50:00 -0800

    Profile edit page bs4 updates

--- config/initializers/simple_form_bootstrap.rb
+++ config/initializers/simple_form_bootstrap.rb
@@ -99,13 +99,13 @@ SimpleForm.setup do |config|
     end
   end
 
-  config.wrappers :horizontal_input_group, tag: 'div', class: 'form-group', error_class: 'has-error' do |b|
+  config.wrappers :horizontal_input_group, tag: 'div', class: 'form-group row', error_class: 'has-error' do |b|
     b.use :html5
     b.use :placeholder
-    b.use :label, class: 'col-sm-3 control-label'
+    b.use :label, class: 'col-md-3 control-label'
 
-    b.wrapper tag: 'div', class: 'col-sm-9' do |ba|
-      ba.wrapper tag: 'div', class: 'input-group col-sm-12' do |append|
+    b.wrapper tag: 'div', class: 'col-md-9' do |ba|
+      ba.wrapper tag: 'div', class: 'input-group col-md-12' do |append|
         append.use :input, class: 'form-control'
       end
       ba.use :error, wrap_with: { tag: 'span', class: 'help-block' }
