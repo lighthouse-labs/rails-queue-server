ef969e4b5083e3ce2376dc4defa6f1a67f088c7e
krsnachandra <krsna.c@gmail.com>
2018-04-13 13:45:57 -0700

    #592 - fixed get index test

--- spec/controllers/admin/cohorts_controller_spec.rb
+++ spec/controllers/admin/cohorts_controller_spec.rb
@@ -7,13 +7,14 @@ describe Admin::CohortsController do
   end
 
   describe 'GET #index' do
-    it 'assigns all cohorts to @cohorts minus active cohort' do
+    it 'assigns all cohorts to @cohorts' do
       cohorts = create_list(:cohort, 5)
       get :index
       expect(assigns(:cohorts)).to match_array(cohorts)
     end
 
     it 'renders index template' do
+      cohorts = create_list(:cohort, 5)
       get :index
       expect(response).to render_template :index
     end
