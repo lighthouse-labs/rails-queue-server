024d1320f47e425f8bfead711c47648cc5db284c
David Mills <dave@1morepx.com>
2018-05-04 17:13:58 -0400

    Preventing multiple renders in the calendars controller

--- app/controllers/wallboard/calendars_controller.rb
+++ app/controllers/wallboard/calendars_controller.rb
@@ -1,6 +1,6 @@
 class Wallboard::CalendarsController < Wallboard::BaseController
   def index
-    render json: { error: "Unable to find location #{location_params}" }, status: 422 unless location
+    return render json: { error: "Unable to find location #{location_params}" }, status: 422 unless location
 
     if location.calendar then
       render json: { calendar: { id: location.calendar } }
