28531c646868d8c1bc7c85cfbddb282f92816ed9
Khurram Virani <kvirani@gmail.com>
2018-05-09 09:46:00 -0700

    Fixed the broken helper spec by stubbing current_user

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,14 +1,17 @@
 require "rails_helper"
 
-describe ApplicationHelper do
+describe ApplicationHelper, type: :helper do
+
+  let(:user) { build :user }
 
   describe '#integer_time_to_s' do
     it "should calculate the correct time (heh)" do
-      expect(integer_time_to_s(900)).to eql "9:00"
-      expect(integer_time_to_s(1100)).to eql "11:00"
-      expect(integer_time_to_s(930)).to eql "9:30"
-      expect(integer_time_to_s(1130)).to eql "11:30"
-      expect(integer_time_to_s(959)).to eql "9:59"
+      allow(helper).to receive(:current_user).and_return(user)
+      expect(helper.integer_time_to_s(900)).to eql "9:00"
+      expect(helper.integer_time_to_s(1100)).to eql "11:00"
+      expect(helper.integer_time_to_s(930)).to eql "9:30"
+      expect(helper.integer_time_to_s(1130)).to eql "11:30"
+      expect(helper.integer_time_to_s(959)).to eql "9:59"
     end
   end
 end
