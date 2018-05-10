e8c8106a174b8ce15d3e6dcbf578c2dd5e753bbf
krsnachandra <krsna.c@gmail.com>
2018-05-10 10:34:30 -0700

    #612 - refactoring as per PR review by KV

--- app/interactors/assign_as_student_to_cohort.rb
+++ app/interactors/assign_as_student_to_cohort.rb
@@ -3,9 +3,10 @@ class AssignAsStudentToCohort
   include Interactor
 
   def call
-    context.user.cohort = context.cohort
-    context.user.type = 'Student'
-    context.user.save!(validate: false)
+    @user = context.user
+    @user.cohort = context.cohort
+    @user.type = 'Student'
+    @user.save!(validate: false)
   end
 
 end
