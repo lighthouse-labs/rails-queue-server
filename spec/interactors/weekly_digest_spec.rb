b2c04e2be9b0753c832dc2428209da5373d3b9f9
krsnachandra <krsna.c@gmail.com>
2018-05-28 13:50:42 -0700

    #611 - test for weekly digest

---
+++ spec/interactors/weekly_digest_spec.rb
@@ -0,0 +1,27 @@
+require "rails_helper"
+
+describe WeeklyDigest do
+  program = Program.find_or_create_by(name: "Bootcamp") do |p|
+    p.weeks = 10
+    p.days_per_week = 5
+    p.weekends = true
+    p.curriculum_unlocking = 'weekly'
+    p.has_projects = true
+    p.has_interviews = true
+    p.has_code_reviews = true
+    p.has_queue = true
+  end
+
+  subject(:context) { WeeklyDigest.call(program: program) }
+
+  describe ".call" do
+
+    context "when given a valid program" do
+      it "successfully sends an email to the curriculum team" do
+        expect(context).to be_a_success
+      end
+    end
+
+  end
+  
+end
