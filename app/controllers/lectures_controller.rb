dc98774ad5987542906ce5401dcdee8dae32c309
krsnachandra <krsna.c@gmail.com>
2018-06-01 15:21:03 -0700

    created lectures controller

---
+++ app/controllers/lectures_controller.rb
@@ -0,0 +1,12 @@
+class LecturesController < ApplicationController
+
+  # implement before_action to make sure this is locked if the student isn't on the day yet
+  # DRY the code with before_action to load @activity and @lecture for the routes that need them
+
+  def show
+    @activity = Activity.find(params[:activity_id])
+    @lecture = Lecture.find(params[:id])
+    # @day = CurriculumDay.new(@lecture.day, @lecture.cohort)
+  end
+
+end
