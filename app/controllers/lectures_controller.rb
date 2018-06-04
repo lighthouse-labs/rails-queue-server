23f71570716a3452c94f042145141001432a9da7
krsnachandra <krsna.c@gmail.com>
2018-06-04 12:49:59 -0700

    lecture show page created

--- app/controllers/lectures_controller.rb
+++ app/controllers/lectures_controller.rb
@@ -3,6 +3,8 @@ class LecturesController < ApplicationController
   # implement before_action to make sure this is locked if the student isn't on the day yet
   # DRY the code with before_action to load @activity and @lecture for the routes that need them
 
+  include CourseCalendar
+
   def show
     @activity = Activity.find(params[:activity_id])
     @lecture = Lecture.find(params[:id])
