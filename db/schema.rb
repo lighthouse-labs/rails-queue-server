5dd9945669c66015c3c5c61c56320135de431cd5
krsnachandra <krsna.c@gmail.com>
2018-06-12 11:35:40 -0700

    merged master into branch

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20180607174754) do
-=======
 ActiveRecord::Schema.define(version: 20180601004248) do
->>>>>>> master
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
