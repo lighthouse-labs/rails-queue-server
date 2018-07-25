be494a54a3a1622826a5b913241c1117e09914d5
Khurram Virani <kvirani@gmail.com>
2018-07-25 16:35:17 -0700

    inconsequential changes to schena

--- db/schema.rb
+++ db/schema.rb
@@ -681,8 +681,8 @@ ActiveRecord::Schema.define(version: 20180620235023) do
     t.datetime "created_at",                      null: false
     t.datetime "updated_at",                      null: false
     t.string   "unlock_on_day",         limit: 5
-    t.text     "description"
     t.boolean  "public"
+    t.text     "description"
     t.index ["content_repository_id"], name: "index_workbooks_on_content_repository_id", using: :btree
     t.index ["slug"], name: "index_workbooks_on_slug", unique: true, using: :btree
     t.index ["uuid"], name: "index_workbooks_on_uuid", unique: true, using: :btree
