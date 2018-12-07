c96b8de5c361ea15eef6fe9e72b6b20c22fd9b82
Khurram Virani <kvirani@gmail.com>
2018-12-06 22:27:21 -0800

    minor UI improvement: dont need heading above queue

--- app/assets/javascripts/components/queue/location.js.jsx
+++ app/assets/javascripts/components/queue/location.js.jsx
@@ -9,13 +9,11 @@ window.Queue.Location = class Location extends React.Component {
     const loc = this.props.location;
     return (
       <div className="row queue-by-location">
-        <div className="col-md-12">
-          <h4>{loc.name} Queue</h4>
-        </div>
         <div className="col-lg-6">
           <Queue.OpenRequestsList requests={loc.requests || []} />
           <Queue.InProgressList assistances={loc.assistances || []} evaluations={loc.inProgressEvaluations || []} interviews={loc.inProgressInterviews || []} />
           <Queue.PendingEvaluationsList evaluations={loc.pendingEvaluations || []} />
+          <Queue.InterviewStatusList cohorts={loc.interviewStatusesByCohort || []} students={loc.students} />
         </div>
         <div className="col-lg-6">
           <Queue.StudentsList students={loc.students} />
