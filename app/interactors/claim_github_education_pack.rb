fdcb5c961716150d264799041c58a84e610d6a31
Khurram Virani <kvirani@gmail.com>
2019-03-06 15:21:19 -0800

    Fix warning from rubocop about URI escape

--- app/interactors/claim_github_education_pack.rb
+++ app/interactors/claim_github_education_pack.rb
@@ -11,7 +11,8 @@ class ClaimGithubEducationPack
     school_id = ENV['GITHUB_EDUCATION_SCHOOL_ID']
     secret_key = ENV['GITHUB_EDUCATION_SECRET_KEY']
     signature = OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha256'), secret_key, school_id + student_id)
-    context.url = "https://education.github.com/student/verify?school_id=#{school_id}&student_id=#{URI.escape(student_id)}&signature=#{URI.escape(signature)}"
+
+    context.url = "https://education.github.com/student/verify?school_id=#{school_id}&student_id=#{URI.encode_www_form_component(student_id)}&signature=#{URI.encode_www_form_component(signature)}"
 
     unless @user.update(github_education_action: 'claimed', github_education_action_at: Time.current)
       context.fail!(error: @user.errors.full_messages.first)
