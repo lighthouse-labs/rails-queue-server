d9634d34edc4f3c98e1629b6f47e175f79f3b675
Khurram Virani <kvirani@gmail.com>
2019-04-21 12:06:02 -0700

    Add more charts
    - Added Queue stats (charts only for now)
    - Cleaned up and add more Charts to Curiculum Feedbacks page

--- config/initializers/date_formats.rb
+++ config/initializers/date_formats.rb
@@ -5,3 +5,18 @@ Time::DATE_FORMATS[:friendly] = ->(date) { date.strftime("%B #{date.day.ordinali
 
 Time::DATE_FORMATS[:date_and_time] = ->(date) { date.strftime("%B #{date.day.ordinalize} %Y %H:%M") }
 Time::DATE_FORMATS[:short_friendly] = ->(date) { date.strftime("%b #{date.day}") }
+
+Date::DATE_FORMATS[:quarter] = Time::DATE_FORMATS[:quarter] = ->(date) {
+  case [date.day, date.month]
+  when [1, 1]
+    date.strftime("Q1 %Y")
+  when [1, 4]
+    date.strftime("Q2 %Y")
+  when [1, 7]
+    date.strftime("Q3 %Y")
+  when [1, 10]
+    date.strftime("Q4 %Y")
+  else
+    date.strftime("Q? %Y")
+  end
+}
