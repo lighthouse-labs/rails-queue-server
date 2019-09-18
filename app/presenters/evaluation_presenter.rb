8daae962cca9fed484686b712500cb314e21422f
Travis  Borsa <travisborsa@Traviss-MacBook-Air.local>
2019-09-18 15:40:15 -0700

    Fixed linting problems, and used boolean completed_at? method.

--- app/presenters/evaluation_presenter.rb
+++ app/presenters/evaluation_presenter.rb
@@ -9,25 +9,25 @@ class EvaluationPresenter < BasePresenter
   def project_status
     result = render('projects/student_project_status', project_evaluation: evaluation)
     result += tag('br')
-    if evaluation.completed_at
+    if evaluation.completed_at?
       curriculum_day = CurriculumDay.new(evaluation.completed_at.to_date, evaluation.student.cohort).to_s
       completed = evaluation.completed_at.to_date.to_s
-      result += content_tag(:span, curriculum_day , {class: 'badge badge-success', 'data-toggle': 'tooltip', 'title': "#{time_ago_in_words(evaluation.completed_at)} ago" }) 
+      result += content_tag(:span, curriculum_day, class: 'badge badge-success', 'data-toggle': 'tooltip', 'title': "#{time_ago_in_words(evaluation.completed_at)} ago")
       result += tag('br')
-      result += content_tag(:small, completed )
+      result += content_tag(:small, completed)
     end
-    
+
     content_tag(:div, result)
   end
 
   def date_submitted
     days = evaluation.created_at.to_date - CurriculumDay.new(evaluation.project.end_day, evaluation.student.cohort).date
-    curriculum_day = CurriculumDay.new(evaluation.created_at.to_date, evaluation.student.cohort).to_s;
-    if days > 0
-      result = content_tag(:span, curriculum_day , {class: 'badge badge-danger', 'data-toggle': 'tooltip', 'title': "#{days.to_i} days late" })
-    else
-      result = content_tag(:span, curriculum_day , {class: 'badge badge-success', 'data-toggle': 'tooltip', 'title': "#{days.to_i.abs} days early" })
-    end
+    curriculum_day = CurriculumDay.new(evaluation.created_at.to_date, evaluation.student.cohort).to_s
+    result = if days > 0
+               content_tag(:span, curriculum_day, class: 'badge badge-danger', 'data-toggle': 'tooltip', 'title': "#{days.to_i} days late")
+             else
+               content_tag(:span, curriculum_day, class: 'badge badge-success', 'data-toggle': 'tooltip', 'title': "#{days.to_i.abs} days early")
+             end
     result += tag('br')
     result += content_tag(:small, evaluation.created_at.to_date)
     result
