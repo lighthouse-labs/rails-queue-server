604a3449b016f33aeb53ef9765f0f93cef412819
Khurram Virani <kvirani@gmail.com>
2019-09-19 10:35:11 -0700

    wip - not tested

---
+++ app/interactors/student/merge_accounts.rb
@@ -0,0 +1,33 @@
+class Student::MergeAccounts
+
+    include Interactor
+  
+    before do
+      @from = context.from_account
+      @to   = context.to_account
+    end
+  
+    def call
+      fields = {
+        ActivitySubmission => :user,
+        ActivityFeedback   => :user,
+        ActivityAnswer     => :user,
+        AssistanceRequest  => :requestor_id,
+        Assistance         => :assistee_id,
+        DayFeedback        => :user_id,
+        Evaluation         => :student_id,
+        Feedback           => :student_id,
+        OutcomeResult      => :user_id,
+        PrepAssistanceRequest => :user_id,
+        QuizSubmission     => :user_id,
+        TechInterview      => :interviewee_id
+      }
+
+      fields.each do |model, attribute|
+        model.where(attribute => @from.id).update_all(attribute => @to.id)
+      end
+    
+    end
+  
+  end
+  
