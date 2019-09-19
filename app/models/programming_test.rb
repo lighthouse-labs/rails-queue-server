bf2ea6704563ab4d7a19fa30892ae0dc755091f1
David Mills <dave@1morepx.com>
2019-09-19 13:54:08 -0400

    Update /assessment_tests endpoint to use ProgrammingTest

--- app/models/programming_test.rb
+++ app/models/programming_test.rb
@@ -1,3 +1,9 @@
 class ProgrammingTest < ApplicationRecord
   has_many :test_activities
+
+  scope :active, -> { 
+    includes(:test_activities)
+      .where(activities: { archived: [false, nil] })
+      .order('activities.day')
+    }
 end
