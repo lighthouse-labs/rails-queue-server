d9357a6a5b3c4621998ba770bb6e13cca256ee6c
David Mills <dave@1morepx.com>
2019-10-03 13:21:45 -0400

    Fixing some bad grammar

--- app/interactors/content/load_programming_test.rb
+++ app/interactors/content/load_programming_test.rb
@@ -13,7 +13,7 @@ class Content::LoadProgrammingTest
     d = @data
     uuid = d['uuid']
     abort("\n\n---\nHALT! Test UUID required") if uuid.blank?
-    abort("\n\n---\nHALT! Tests need an exam_code investigate activity with this uuid #{d['uuid']}") if d['exam_code'].nil?
+    abort("\n\n---\nHALT! Tests need an exam_code! investigate activity with this uuid #{d['uuid']}") if d['exam_code'].nil?
 
     programming_test = ProgrammingTest.find_or_initialize_by(uuid: uuid)
     programming_test.assign_attributes(exam_code: d['exam_code'])
@@ -21,4 +21,5 @@ class Content::LoadProgrammingTest
     @records.push programming_test
     context.programming_test = programming_test
   end
+
 end
