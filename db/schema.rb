86dabfac7f63096add11045151f77889e29dac4a
David Mills <dave@1morepx.com>
2019-10-03 13:39:53 -0400

    Better data integrity
    Added null checks in the db columns
    Added AR validations

--- db/schema.rb
+++ db/schema.rb
@@ -418,8 +418,8 @@ ActiveRecord::Schema.define(version: 20191011163600) do
   end
 
   create_table "programming_tests", force: :cascade do |t|
-    t.string   "exam_code"
-    t.string   "uuid"
+    t.string   "exam_code",  null: false
+    t.string   "uuid",       null: false
     t.json     "config"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
