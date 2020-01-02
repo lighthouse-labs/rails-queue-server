ac7592ddb51889e996f5e852438aa30ba60746b8
David Mills <dave@1morepx.com>
2020-01-02 13:08:15 -0500

    Resolves #984, for cohorts older than Jan 1st 2020 show a link to the old deprecated dashboard
    This is as the old dashboard is what contains the data for older cohorts

--- app/helpers/teachers_helper.rb
+++ app/helpers/teachers_helper.rb
@@ -1,2 +1,18 @@
 module TeachersHelper
+  def url_for_proctologist(test, cohort)
+    student_usernames = cohort.students.active.map do |s|
+      if s.rolled_in?(cohort)
+        "#{s.github_username}-1"
+      else
+        s.github_username
+      end
+    end
+    student_usernames = student_usernames.join(',')
+    url = URI("#{cohort.program.proctor_url}/admin/exams/#{test}/?studentIds=#{student_usernames}")
+
+    url.user = "admin"
+    url.password = "Fresnel"
+
+    url.to_s
+  end
 end
