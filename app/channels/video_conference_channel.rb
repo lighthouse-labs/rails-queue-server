7186da81f151d854c255d0aea394522be033702e
tborsa <travis.borsa@gmail.com>
2020-03-20 15:21:48 -0700

    Video conference cable channel

---
+++ app/channels/video_conference_channel.rb
@@ -0,0 +1,27 @@
+class VideoConferenceChannel < ApplicationCable::Channel
+
+  def subscribed
+    stream_from channel_name
+  end
+
+  def update_conference(status, video_conference_id)
+    video_conference = VideoConference.find(video_conference_id)
+    ActionCable.server.broadcast channel_name, type: "VideoConferenceUpdate", object: VideoConferenceSerializer.new(video_conference).as_json
+  end
+
+  def self.update_conference(video_conference, channel)
+    ActionCable.server.broadcast channel, type:   "VideoConferenceUpdate", object: VideoConferenceSerializer.new(video_conference).as_json
+  end
+
+  def self.channel_name_from_cohort(cohort)
+    "video-conferences-#{cohort.name}"
+  end
+
+  protected
+
+  def channel_name
+    cohort_name = cohort.name
+    "video-conferences-#{cohort_name}"
+  end
+
+end
