e1e8d09fc7026f374e23fab4082b6df058b11d1f
tborsa <travis.borsa@gmail.com>
2020-03-20 14:05:59 -0700

    Video conferences controller update action

--- app/controllers/teacher/video_conferences_controller.rb
+++ app/controllers/teacher/video_conferences_controller.rb
@@ -15,9 +15,32 @@ class Teacher::VideoConferencesController < Teacher::BaseController
 
   end
 
+  def update
+    video_conference = VideoConference.find(params[:id])
+
+    zoom_update = true
+    if conference_params[:status] == 'finished' && video_conference.status != 'finished'
+      # use zoom api to finish conference
+      zoom = ZoomMeetings.new
+      zoom_update = zoom.end_meeting(video_conference)
+    elsif conference_params[:status] == 'broadcast' && video_conference.status != 'broadcast'
+      # action cable to update cohort on new conference
+    end
+
+    if zoom_update && video_conference.update_attributes(conference_params)
+      flash[:notice] = "Video Conference Updated"
+    else
+      flash[:alert] = "Video Conferene Could not be Updated"
+    end
+
+    redirect_back fallback_location: root_path
+  end
+
   def create
     
     zoom = ZoomMeetings.new
+    #check if there is already an active conferenect
+    # VideoConference.for_activity(conference_params[:activity_id])&.for_cohort(conference_params[:cohort_id])
 
     # create zoom meeting
     res = zoom.create_meeting(@current_user, conference_params[:start_time], conference_params[:duration], conference_params[:topic])
@@ -30,7 +53,7 @@ class Teacher::VideoConferencesController < Teacher::BaseController
       VideoConference.create()
       conference = VideoConference.new(
         start_time: res['start_time'], 
-        duration: res['start_time'], 
+        duration: res['duration'], 
         status: 'waiting', 
         zoom_meeting_id: res['id'], 
         start_url: res['start_url'], 
@@ -54,7 +77,7 @@ class Teacher::VideoConferencesController < Teacher::BaseController
 
   def conference_params
     params.require(:video_conference).permit(
-      :cohort_id, :activity_id, :topic, :duration, :start_time
+      :cohort_id, :activity_id, :topic, :duration, :start_time, :status
     )
    end
 
