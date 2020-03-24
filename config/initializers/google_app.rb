7d195a14b7584683fc7260653f875a52617fdd92
Khurram Virani <kvirani@gmail.com>
2020-03-24 09:12:17 -0700

    Make config more defensive, otherwise first run will fail (pre migration)

--- config/initializers/google_app.rb
+++ config/initializers/google_app.rb
@@ -1,4 +1,7 @@
-creds = Program.first.google_app_credentials
+# Instead of having these in the .env, due to the multi line private key which causes a bug in dokku that prevents deployment, we've put these secrets into the Programs table. Perhaps they are better there anyway? - KV
+
+# Fails on first run if you dont have the migration run yet, hence using .try
+creds = Program.first.try(:google_app_credentials) || {}
 ENV['GOOGLE_ACCOUNT_TYPE'] ||= creds['GOOGLE_ACCOUNT_TYPE']
 ENV['GOOGLE_PRIVATE_KEY_ID'] ||= creds['GOOGLE_PRIVATE_KEY_ID']
 ENV['GOOGLE_PRIVATE_KEY'] ||= creds['GOOGLE_PRIVATE_KEY']
