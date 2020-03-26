6874f7b2c89954aa75a47c6d545a170e8ed65a6c
tborsa <travis.borsa@gmail.com>
2020-03-26 10:08:15 -0700

    Error handling for invalid google creds

--- app/services/google_hangout.rb
+++ app/services/google_hangout.rb
@@ -5,57 +5,57 @@ class GoogleHangout
 
   def initialize
     @APPLICATION_NAME = "CompassHangoutsMeet".freeze
-    scope = 'https://www.googleapis.com/auth/calendar.events'
-
-    @authorizer = ::Google::Auth::ServiceAccountCredentials.make_creds(
-      scope: scope
-    )
+    @scope = 'https://www.googleapis.com/auth/calendar.events'
   end
 
   def create_hangout(assistor, assistee)
-    begin
+    @authorizer = ::Google::Auth::ServiceAccountCredentials.make_creds(
+      scope: @scope
+    )
+
+    if ENV["GOOGLE_SUB_EMAIL"]
       google_org_domain = ENV["GOOGLE_SUB_EMAIL"].split('@').last
       @authorizer.sub = ENV["GOOGLE_SUB_EMAIL"] if assistee.email.split('@').last == google_org_domain || assistor.email.split('@').last == google_org_domain
-      @authorizer.fetch_access_token!
-
-      service = Google::Apis::CalendarV3::CalendarService.new
-      service.authorization = @authorizer
-
-      now = DateTime.now
-
-      event = Google::Apis::CalendarV3::Event.new(
-        summary:           "Assistance Request Between #{assistor.full_name} and #{assistee.full_name}",
-        location:          (assistee.location&.name).to_s,
-        description:       'This event was automatically created by the HangoutsCreator Service Account',
-        guests_can_modify: true,
-        start:             Google::Apis::CalendarV3::EventDateTime.new(
-          date_time: now.to_s
-        ),
-        end:               Google::Apis::CalendarV3::EventDateTime.new(
-          date_time: now.new_offset('+00:30').to_s
-        ),
-        # attendees: [
-        #   Google::Apis::CalendarV3::EventAttendee.new(
-        #     email: assistor.email
-        #   ),
-        #   Google::Apis::CalendarV3::EventAttendee.new(
-        #     email: assistee.email
-        #   )
-        # ],
-        conference_data:   {
-          create_request: {
-            request_id: SecureRandom.uuid
-          }
-        }
-      )
-
-      result = service.insert_event('primary', event, conference_data_version: 1)
-    rescue StandardError => err
-      puts "Error creating hangout"
-      return nil
     end
 
+    @authorizer.fetch_access_token!
+    service = Google::Apis::CalendarV3::CalendarService.new
+    service.authorization = @authorizer
+
+    now = DateTime.now
+
+    event = Google::Apis::CalendarV3::Event.new(
+      summary:           "Assistance Request Between #{assistor.full_name} and #{assistee.full_name}",
+      location:          (assistee.location&.name).to_s,
+      description:       'This event was automatically created by the HangoutsCreator Service Account',
+      guests_can_modify: true,
+      start:             Google::Apis::CalendarV3::EventDateTime.new(
+        date_time: now.to_s
+      ),
+      end:               Google::Apis::CalendarV3::EventDateTime.new(
+        date_time: now.new_offset('+00:30').to_s
+      ),
+      # attendees: [
+      #   Google::Apis::CalendarV3::EventAttendee.new(
+      #     email: assistor.email
+      #   ),
+      #   Google::Apis::CalendarV3::EventAttendee.new(
+      #     email: assistee.email
+      #   )
+      # ],
+      conference_data:   {
+        create_request: {
+          request_id: SecureRandom.uuid
+        }
+      }
+    )
+
+    result = service.insert_event('primary', event, conference_data_version: 1)
     event_details(result)
+  rescue StandardError => err
+    puts "Error creating hangout"
+    puts err.inspect
+    nil
   end
 
   private
@@ -64,7 +64,7 @@ class GoogleHangout
     entry_points = event&.conference_data&.entry_points
     uri = nil
     entry_points&.each do |point|
-      return {uri: point.uri, type: event&.conference_data&.conference_solution&.key&.type} if point.entry_point_type == 'video'
+      return { uri: point.uri, type: event&.conference_data&.conference_solution&.key&.type } if point.entry_point_type == 'video'
     end
   end
 
