4b8ffe90fa00cd545f4b39fa87b4077737af5795
tborsa <travis.borsa@gmail.com>
2020-04-02 16:50:35 -0700

    Avoid initializer rescue in favor of connection check

--- config/initializers/google_app.rb
+++ config/initializers/google_app.rb
@@ -1,16 +1,12 @@
 # Instead of having these in the .env, due to the multi line private key which causes a bug in dokku that prevents deployment, we've put these secrets into the Programs table. Perhaps they are better there anyway? - KV
 
 # Fails on first run if you dont have the migration run yet, hence using .try
-begin
-  creds = Program.first.try(:settings) if  ActiveRecord::Base.connection.table_exists? 'programs'
-  creds ||= {}
-  google_creds = creds['google_app_credentials'] || {}
-  ENV['GOOGLE_ACCOUNT_TYPE'] ||= google_creds['GOOGLE_ACCOUNT_TYPE']
-  ENV['GOOGLE_PRIVATE_KEY_ID'] ||= google_creds['GOOGLE_PRIVATE_KEY_ID']
-  ENV['GOOGLE_PRIVATE_KEY'] ||= google_creds['GOOGLE_PRIVATE_KEY']
-  ENV['GOOGLE_CLIENT_EMAIL'] ||= google_creds['GOOGLE_CLIENT_EMAIL']
-  ENV['GOOGLE_CLIENT_ID'] ||= google_creds['GOOGLE_CLIENT_ID']
-  ENV['GOOGLE_SUB_EMAIL'] ||= google_creds['GOOGLE_SUB_EMAIL']
-rescue
-  puts "Can't config google app db connection not established"
-end
+creds = Program.first.try(:settings) if  ActiveRecord::Base.connected? && ActiveRecord::Base.connection.table_exists? 'programs'
+creds ||= {}
+google_creds = creds['google_app_credentials'] || {}
+ENV['GOOGLE_ACCOUNT_TYPE'] ||= google_creds['GOOGLE_ACCOUNT_TYPE']
+ENV['GOOGLE_PRIVATE_KEY_ID'] ||= google_creds['GOOGLE_PRIVATE_KEY_ID']
+ENV['GOOGLE_PRIVATE_KEY'] ||= google_creds['GOOGLE_PRIVATE_KEY']
+ENV['GOOGLE_CLIENT_EMAIL'] ||= google_creds['GOOGLE_CLIENT_EMAIL']
+ENV['GOOGLE_CLIENT_ID'] ||= google_creds['GOOGLE_CLIENT_ID']
+ENV['GOOGLE_SUB_EMAIL'] ||= google_creds['GOOGLE_SUB_EMAIL']
