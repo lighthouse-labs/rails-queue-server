4c5ef440dbcafaaf2143df9a7ecd38e01d87a242
tborsa <travis.borsa@gmail.com>
2020-04-13 12:33:53 -0700

    new zoom interactors for acting on the VideoConference model

---
+++ app/interactors/zoom_meeting/push_license_add.rb
@@ -0,0 +1,15 @@
+
+class ZoomMeeting::PushLicenseAdd
+
+  include Interactor
+
+  before do
+    @user = context.user
+  end
+
+  def call
+    context.update_user_stack ||= []
+    context.update_user_stack.push(user: @user, license: 2) if @user
+  end
+
+end
