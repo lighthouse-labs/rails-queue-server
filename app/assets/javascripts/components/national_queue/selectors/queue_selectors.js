de340cc67908d97a0dbef348bf4fea71d8a75bb1
tborsa <travis.borsa@gmail.com>
2020-04-14 17:20:13 -0700

    National Queue selectors

---
+++ app/assets/javascripts/components/national_queue/selectors/queue_selectors.js
@@ -0,0 +1,24 @@
+const selectActive = (tasks) => {
+ return tasks.filter((task) => {
+  return true
+ })
+}
+
+const selectPending = (tasks) => {
+  return tasks.filter((task) => {
+    return true
+  })
+}
+
+const selectInProgress = (tasks) => {
+  return tasks.filter((task) => {
+    return true
+  })
+}
+
+
+window.NationalQueue.QueueSelectors =  {
+  selectActive,
+  selectPending,
+  selectInProgress
+}
