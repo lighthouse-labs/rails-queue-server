97580642b17924ee7c17aa6a319b9601df13d5c4
tborsa <travis.borsa@gmail.com>
2020-04-21 11:36:21 -0700

    Selectors update to use socket updates

--- app/assets/javascripts/components/national_queue/selectors/queue_selectors.js
+++ app/assets/javascripts/components/national_queue/selectors/queue_selectors.js
@@ -1,26 +1,48 @@
 window.NationalQueue = window.NationalQueue || {};
 
-const selectActive = (tasks) => {
+const selectOpen = (tasks, user) => {
  return tasks.filter((task) => {
-  return true
+  return (task.teacher && task.teacher.id === user.id) && task.state === 'pending' && task.type !== 'ProjectEvaluation';
  })
 }
 
 const selectPending = (tasks) => {
+  console.log('select pending', tasks);
   return tasks.filter((task) => {
-    return true
+    console.log('task', task.state, task.type)
+    return task.state === 'pending' && task.type === 'ProjectEvaluation';
   })
 }
 
 const selectInProgress = (tasks) => {
   return tasks.filter((task) => {
-    return true
+    return task.state === 'in_progress';
   })
 }
 
+const tasksWithUpdates = (tasks, updates) => {
+  tasks = [...tasks];
+  for (update of updates) {
+    let updated = false;
+    for ([index, task] of tasks.entries()) {
+      if (task.id === update.object.id && task.type === update.object.type) {
+        tasks[index] = update.object;
+        updated = true;
+        break;
+      }
+    }
+    if (!updated) {
+      tasks.push(update.object);
+    }
+  }
+  return Array.from(_(tasks).sortBy((item) => {
+    return (item.startAt || item.startedAt || item.createdAt)
+  }).reverse());
+}
 
 window.NationalQueue.QueueSelectors =  {
-  selectActive,
+  selectOpen,
   selectPending,
-  selectInProgress
+  selectInProgress,
+  tasksWithUpdates
 }
