ad5bcf7a2e2b58a0ab472c634c6e22c715261901
Khurram Virani <kvirani@gmail.com>
2020-05-01 09:03:19 -0700

    Various code and UX fixes with workbook-based lecture
    - params[:workbook] was being set but not used
    - Important Fix: left nav was not showing workbook state when creating, editing, or viewing a lecture note
    - used `activity.bootcamp?` where applicable
    
    outstanding issue:
    
    - workbook context will be lost in the email link sent to students. Need to fix that still

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200414172242) do
+ActiveRecord::Schema.define(version: 20200415153940) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
