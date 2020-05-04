50b48ffafdee6a6acca662355da508204c73873a
tborsa <travis.borsa@gmail.com>
2020-05-04 10:03:01 -0700

    Broadcast teacher status to national queue channel

--- app/interactors/national_queue/off_duty.rb
+++ app/interactors/national_queue/off_duty.rb
@@ -3,6 +3,7 @@ class NationalQueue::OffDuty
   include Interactor::Organizer
 
   organize  NationalQueue::SmartTaskReassignQueue,
+            NationalQueue::BroadcastTeacherAvailability,
             NationalQueue::BroadcastTeacherQueueUpdate
 
 end
--- app/interactors/national_queue/on_duty.rb
+++ app/interactors/national_queue/on_duty.rb
@@ -3,6 +3,7 @@ class NationalQueue::OnDuty
   include Interactor::Organizer
 
   organize  NationalQueue::SmartTaskAssignQueue,
+            NationalQueue::BroadcastTeacherAvailability,
             NationalQueue::BroadcastTeacherQueueUpdate
 
 end
