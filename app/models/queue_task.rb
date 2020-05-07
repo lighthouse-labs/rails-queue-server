083601795f3aa4984ad18ef2aff36f2d6816e942
tborsa <travis.borsa@gmail.com>
2020-05-07 10:34:52 -0700

    Fix queue task in-progress check

--- app/models/queue_task.rb
+++ app/models/queue_task.rb
@@ -21,7 +21,7 @@ class QueueTask < ApplicationRecord
   def state
     if assistance_request.open?
       'pending'
-    elsif assistance_request.in_progress? && user === assistance_request.assistance&.assistor
+    elsif assistance_request.in_progress? && user.id == assistance_request.assistance&.assistor.id
       'in_progress'
     else
       'closed'
