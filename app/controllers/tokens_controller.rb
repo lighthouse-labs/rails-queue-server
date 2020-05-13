672fa1624da4eaeba74591bbde4225146548cf2b
tborsa <travis.borsa@gmail.com>
2020-05-13 14:58:06 -0700

    Fix tokens controller parameter validation

--- app/controllers/tokens_controller.rb
+++ app/controllers/tokens_controller.rb
@@ -3,7 +3,9 @@ class TokensController < ApplicationController
   before_action :auth_compass_instance
 
   def create
-    payload = { user_uid: token_params[:user_uid], compass_instance_id: @compass_instance.id }
+    payload = token_params[:user].to_h
+    payload[:compass_instance_id] = @compass_instance.id
+    payload[:access] ||= []
 
     # token secret should be rotating and not stored in env
     token = JWT.encode payload, ENV['TOKEN_SECRET'], 'HS256'
@@ -13,13 +15,14 @@ class TokensController < ApplicationController
   private
 
   def token_params
-    params.require(:token_options).permit(:key, :secret, :user_uid)
+    all_options = params.require(:token_options)[:user].try(:permit!)
+    params.require(:token_options).permit(:key, :secret).merge(:user => all_options)
   end
 
   def auth_compass_instance
     @compass_instance = CompassInstance.find_by(key: token_params[:key])
     Octopus.using(@compass_instance.name) do
-      @user = User.find_by(uid: token_params[:user_uid])
+      @user = User.find_by(uid: token_params['user'].try(:[], :uid))
       if !@compass_instance || BCrypt::Password.new(@compass_instance&.secret) != token_params[:secret]
         render json: { error: 'Incorrect key or secret.' }
       elsif !@user
