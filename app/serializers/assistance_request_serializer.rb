a39a0763498964f48acd1a495b78694da2ccae4d
tborsa <travis.borsa@gmail.com>
2020-05-14 14:12:51 -0700

    Refactor assistance serializer

--- app/serializers/assistance_request_serializer.rb
+++ app/serializers/assistance_request_serializer.rb
@@ -1,9 +1,21 @@
 class AssistanceRequestSerializer < ActiveModel::Serializer
 
+  root false
   format_keys :lower_camel
-  attributes :id, :reason, :start_at, :position_in_queue
+  attributes :id, :requestor, :request, :start_at, :canceled_at, :type
 
-  has_one :activity, serializer: ActivitySerializer
-  has_one :requestor, serializer: UserSerializer
+  # has_one :compass_instance, serializer: CompassInstanceSerializer
+  delegate :state, to: :object
 
+  def assistor
+    UserSerializer.new(object.assistance.assistor).as_json if object.assistance&.assistor
+  end
+
+  def conference_link
+    object.assistance&.conference_link
+  end
+
+  def assistance_start
+    object.assistance&.start_at
+  end
 end
