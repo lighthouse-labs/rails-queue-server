6a583e04666593ffdbc4489686a39310605b21b7
tborsa <travis.borsa@gmail.com>
2020-05-20 09:30:01 -0700

    Add assistor to assistance serializer

--- app/serializers/assistance_serializer.rb
+++ app/serializers/assistance_serializer.rb
@@ -2,6 +2,10 @@ class AssistanceSerializer < ActiveModel::Serializer
 
   root false
 
-  attributes :id, :start_at, :end_at, :notes, :rating, :student_notes, :flag, :conference_link, :conference_type
+  attributes :id, :start_at, :end_at, :notes, :rating, :student_notes, :flag, :conference_link, :conference_type, :assistor
+
+  def assistor
+    UserSerializer.new(object.assistor)
+  end
 
 end
