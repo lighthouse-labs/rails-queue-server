c3152779d889f88498c55bba7a8b7d39d2652b54
tborsa <travis.borsa@gmail.com>
2020-05-29 12:54:31 -0700

    Handling for invalid custom_avatar

--- app/helpers/avatar_helper.rb
+++ app/helpers/avatar_helper.rb
@@ -2,7 +2,7 @@ module AvatarHelper
 
   def avatar_for(user)
     if user&.custom_avatar?
-      user.custom_avatar.url(:thumb)
+      user.custom_avatar.try(:url, :thumb) || user.custom_avatar
     elsif user&.avatar_url
       user.avatar_url
     else
