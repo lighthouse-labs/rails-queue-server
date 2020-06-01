f696d091026d881d665b061dfc3641418ae3a4b6
tborsa <travis.borsa@gmail.com>
2020-06-01 13:44:30 -0700

    Avatar helper fix

--- app/helpers/avatar_helper.rb
+++ app/helpers/avatar_helper.rb
@@ -1,9 +1,7 @@
 module AvatarHelper
 
   def avatar_for(user)
-    if user&.custom_avatar?
-      user.custom_avatar.try(:url, :thumb) || user.custom_avatar
-    elsif user&.avatar_url
+    if user&.avatar_url
       user.avatar_url
     else
       image_path('duck-on.png')
