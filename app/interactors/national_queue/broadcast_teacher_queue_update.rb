31c77be482ec354bf82cb6b4ca2fe9a407d6b081
tborsa <travis.borsa@gmail.com>
2020-06-03 13:31:22 -0700

    non admin broadcast bug

--- app/interactors/national_queue/broadcast_teacher_queue_update.rb
+++ app/interactors/national_queue/broadcast_teacher_queue_update.rb
@@ -12,7 +12,10 @@ class NationalQueue::BroadcastTeacherQueueUpdate
       if update[:shared]
         NationalQueueChannel.broadcast 'teacher-national-queue', type: "queueUpdate", object: QueueTaskSerializer.new(task).as_json
       else
-        NationalQueueChannel.broadcast_to task.assistor, type: "queueUpdate", object: QueueTaskSerializer.new(task).as_json
+        assistor = task.assistor
+        # or if task was not routed
+        assistor ||= task.assistance_request&.assistance&.assistor
+        NationalQueueChannel.broadcast_to assistor, type: "queueUpdate", object: QueueTaskSerializer.new(task).as_json
         NationalQueueChannel.broadcast 'admin-national-queue', type: "queueUpdate", object: QueueTaskSerializer.new(task).as_json
       end
     end
